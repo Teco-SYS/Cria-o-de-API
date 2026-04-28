7480223667351789b47f8d1c6eaab903bff0a6e1
Mateus Rodrigues <gdmateus55@gmail.com>
2026-04-27 22:21:09 -0300

    criação do endpoint listagem de usuários

--- src/Server.js
+++ src/Server.js
@@ -6,7 +6,21 @@ function callbackDaraiz( request, response) {
     response.send("API aeromobilistica, Selva, Leões de guerraaaaaa, pantanal")
 }
 
+function CallBackListaUsuarios( request, response ) {
+   
+    const ListaUsuarios = [
+         {
+            Id: 1,
+            Name: "Mateus"
+         } 
+    ]
+
+    response.json(ListaUsuarios)
+}
+
 app.get("/", callbackDaraiz)
+
+app.get("/users", CallBackListaUsuarios)
 
 app.listen(3000, () => {
     console.log("Servidor Online em http://localhost:3000")
